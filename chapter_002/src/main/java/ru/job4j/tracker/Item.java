a4fb53e0c39e33c4c3594aa2a726e9039fb01d23
Alex Konotop <mailto:a.konotop@gmail.com>
2018-08-08 22:28:17 +0700

    1. Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,62 @@
+package ru.job4j.tracker;
+
+public class Item {
+
+    private String id;
+    private String name;
+    private String desc;
+    private String comment;
+    private long created;
+
+    public Item() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return  this.name;
+    }
+
+    public void setName (String id) {
+        this.name = name;
+    }
+
+    public  String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public  String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Item(String name, String desc, long created) {
+       this.name = name;
+       this.desc = desc;
+       this.created = created;
+    }
+
+    public  long getCreated() {
+        return this.created;
+    }
+
+    public void setCreated(){
+        this.created = created;
+    }
+
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,97 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Tracker {
+
+    /**
+     * Массив для хранение заявок.
+     */
+    private Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    private static final Random RN = new Random();
+
+    /**
+     * Метод реализаущий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+
+    public Item add(Item item) {
+        item.setId( this.generateId() );
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+            return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        for (int index = 0; index != this.items.length; index++) {
+            if (this.items[index] != null && this.items[index].getId().equals(id)) {
+                this.items[index] = item;
+                this.items[index].setId(id);
+                result = true;
+                break;
+                }
+        }
+        return result;
+    }
+
+    public void delete(String id) {
+        int unique = items.length;
+        for(int index = 0; index < unique; index++) {
+            if(this.items[index].getId().equals(items[index])){
+                items[index] = items[unique - 1];
+                unique --;
+                index --;
+                Arrays.copyOf(items, unique);
+                break;
+            }
+        }
+}
+
+
+    public Item[] getAll() {
+        Item[] result = new Item[this.position];
+        for (int index = 0; index != this.position; index++) {
+            result[index] = this.items[index];
+        }
+        return result;
+    }
+
+    public Item findByName(String key) {
+        Item result = null;
+        for (Item item : this.items) {
+            if (item != null && item.getName().equals(key)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
