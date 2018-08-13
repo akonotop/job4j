5a3d912e44a959ad41ea7abaeb8c72dbaef1e0fb
Alex Konotop <mailto:a.konotop@gmail.com>
2018-08-13 10:56:19 +0700

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя

--- chapter_002/src/test/java/ru/job4j/tracker/TestTracker.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TestTracker.java
@@ -14,18 +14,18 @@ public class TestTracker {
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("test1","testDescription",123L);
+        Item item = new Item("test1", "testDescription", 123L);
         tracker.add(item);
         assertThat(tracker.findAll()[0], is(item));
     }
     @Test
     public void whenReplaceNameThenReturnNewName() {
         Tracker tracker = new Tracker();
-        Item previous = new Item("test1","testDescription",123L);
+        Item previous = new Item("test1", "testDescription", 123L);
         // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(previous);
         // Создаем новую заявку.
-        Item next = new Item("test2","testDescription2",1234L);
+        Item next = new Item("test2", "testDescription2", 1234L);
         // Проставляем старый id из previous, который был сгенерирован выше.
         next.setId(previous.getId());
         // Обновляем заявку в трекере.
