cb1e9018f073b0b5d76d0a807923acd40d99c3c6
Alex Konotop <mailto:a.konotop@gmail.com>
2018-09-02 13:02:50 +0700

    Кофемашина - переписал алгоритм

--- chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
@@ -1,18 +1,30 @@
 package ru.job4j.coffeeMachine;
 
+import java.util.ArrayList;
+
 public class CoffeeMachine {
-    public static int[] changes(int value, int price) {
-        int amount = 0;
+    public ArrayList<Integer> changes(int value, int price) {
         int change = value - price;
-        if(change > 10 && change%2 == 1 ) {
-            amount = change / 10;
+        ArrayList<Integer> changes = new ArrayList<>();
+        while (change !=0) {
+            if (change > 9) {
+                change = change - 10;
+                changes.add(10);
+            }
+            if (change > 4 && change < 10) {
+                change = change - 5;
+                changes.add(5);
+            }
+            if (change == 3) {
+                change = change - 2;
+                changes.add(2);
+            }
+            if (change == 1) {
+                change = change - 1;
+                changes.add(1);
+            }
         }
-        int i;
-        int[] changes = new int[amount + 1];
-        for(i = 0; i < amount; i++) {
-            changes[i] = 10;
-        }
-        changes[i] = 5;
         return changes;
     }
+
 }
--- chapter_002/src/test/java/ru/job4j/coffeeMachine/CoffeeMachineTest.java
+++ chapter_002/src/test/java/ru/job4j/coffeeMachine/CoffeeMachineTest.java
@@ -3,14 +3,30 @@ package ru.job4j.coffeeMachine;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertThat;
 
 public class CoffeeMachineTest {
     @Test
     public void when50Change15() {
         CoffeeMachine changesTest = new CoffeeMachine();
-        int[] result = changesTest.changes(50, 35);
-        int[] expect = {10, 5};
+        ArrayList<Integer> result = changesTest.changes(50, 35);
+        ArrayList<Integer> expect =new ArrayList<Integer>(){{add(10); add(5);}};
+        assertThat(result, Matchers.is(expect));
+    }
+    @Test
+    public void when73Change38() {
+        CoffeeMachine changesTest = new CoffeeMachine();
+        ArrayList<Integer> result = changesTest.changes(73, 35);
+        ArrayList<Integer> expect =new ArrayList<Integer>(){{add(10); add(10);add(10); add(5); add(2); add(1);}};
+        assertThat(result, Matchers.is(expect));
+    }
+    @Test
+    public void checkChangeValue35Price35() {
+        CoffeeMachine changesTest = new CoffeeMachine();
+        ArrayList<Integer> result = changesTest.changes(35, 35);
+        ArrayList<Integer> expect =new ArrayList<Integer>(){};
         assertThat(result, Matchers.is(expect));
     }
 }
