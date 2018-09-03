615e0020d16399314e7ecbd159467ad20c1b2fd4
Alex Konotop <mailto:a.konotop@gmail.com>
2018-09-03 19:04:06 +0700

    Кофемашина - изменил на List

--- chapter_002/src/test/java/ru/job4j/coffeeMachine/CoffeeMachineTest.java
+++ chapter_002/src/test/java/ru/job4j/coffeeMachine/CoffeeMachineTest.java
@@ -4,6 +4,8 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertThat;
 
@@ -12,21 +14,21 @@ public class CoffeeMachineTest {
     public void when50Change15() {
         CoffeeMachine changesTest = new CoffeeMachine();
         ArrayList<Integer> result = changesTest.changes(50, 35);
-        ArrayList<Integer> expect =new ArrayList<Integer>(){{add(10); add(5);}};
+        List<Integer> expect = Arrays.asList(10, 5);
         assertThat(result, Matchers.is(expect));
     }
     @Test
     public void when73Change38() {
         CoffeeMachine changesTest = new CoffeeMachine();
         ArrayList<Integer> result = changesTest.changes(73, 35);
-        ArrayList<Integer> expect =new ArrayList<Integer>(){{add(10); add(10);add(10); add(5); add(2); add(1);}};
+        List<Integer> expect = Arrays.asList(10, 10, 10, 5, 2, 1);
         assertThat(result, Matchers.is(expect));
     }
     @Test
     public void checkChangeValue35Price35() {
         CoffeeMachine changesTest = new CoffeeMachine();
         ArrayList<Integer> result = changesTest.changes(35, 35);
-        ArrayList<Integer> expect =new ArrayList<Integer>(){};
+        List<Integer> expect = Arrays.asList();
         assertThat(result, Matchers.is(expect));
     }
 }
