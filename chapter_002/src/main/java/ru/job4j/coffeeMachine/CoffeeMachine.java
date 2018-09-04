8600b62455a10451e0011eaa14a2a48d37b95ff2
Alex Konotop <mailto:a.konotop@gmail.com>
2018-09-04 21:59:37 +0700

    Кофемашина - вынес номиналы в масив

--- chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 
 public class CoffeeMachine {
     public ArrayList<Integer> changes(int value, int price) {
+        int[] nominal = {1, 2, 5, 10};
         int change = value - price;
         ArrayList<Integer> changes = new ArrayList<>();
         while (change !=0) {
             if (change > 9) {
-                change = change - 10;
-                changes.add(10);
+                change = change - nominal[3];
+                changes.add(nominal[3]);
             }
             if (change > 4 && change < 10) {
-                change = change - 5;
-                changes.add(5);
+                change = change - nominal[2];
+                changes.add(nominal[2]);
             }
             if (change == 3) {
-                change = change - 2;
-                changes.add(2);
+                change = change - nominal[1];
+                changes.add(nominal[1]);
             }
             if (change == 1) {
-                change = change - 1;
-                changes.add(1);
+                change = change - nominal[0];
+                changes.add(nominal[0]);
             }
         }
         return changes;
