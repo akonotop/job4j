53c3464877ab8de438fbee4b0583302b49f699d0
Alex Konotop <mailto:a.konotop@gmail.com>
2018-09-05 19:43:48 +0700

    2. Написать программу преобразования List в Map.

---
+++ chapter_003/src/main/java/listToMap/UserConvert.java
@@ -0,0 +1,14 @@
+package listToMap;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class UserConvert {
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> result = new HashMap<>();
+        for (User user: list) {
+            result.put(user.getId(), user);
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/listToMap/UserConvertTest.java
@@ -0,0 +1,25 @@
+package listToMap;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+
+import static org.junit.Assert.assertThat;
+
+public class UserConvertTest {
+    @Test
+    public void whenListTohMap() {
+        UserConvert userConvert = new UserConvert();
+        User user1 = new User(1, "Ivan", "Chelyabinsk");
+        User user2 = new User(2, "Igor", "Moscow");
+        List<User> users = Arrays.asList(user1, user2);
+        HashMap<Integer, User> expect = new HashMap<Integer, User>();
+        expect.put(user1.getId(), user1);
+        expect.put(user2.getId(), user2);
+        HashMap<Integer, User> result = userConvert.process(users);
+        assertThat(expect, is(result));
+    }
+}
