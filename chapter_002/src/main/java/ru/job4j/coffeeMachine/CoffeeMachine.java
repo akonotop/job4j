9904b46a8eb3e219ce770d48d928ebd4012d2eea
Alex Konotop <mailto:a.konotop@gmail.com>
2018-09-05 16:44:43 +0700

    Кофемашина - переделал алгоритм

--- chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
@@ -4,25 +4,13 @@ import java.util.ArrayList;
 
 public class CoffeeMachine {
     public ArrayList<Integer> changes(int value, int price) {
-        int[] nominal = {1, 2, 5, 10};
+        int[] coins = {10, 5, 2, 1};
         int change = value - price;
         ArrayList<Integer> changes = new ArrayList<>();
-        while (change !=0) {
-            if (change > 9) {
-                change = change - nominal[3];
-                changes.add(nominal[3]);
-            }
-            if (change > 4 && change < 10) {
-                change = change - nominal[2];
-                changes.add(nominal[2]);
-            }
-            if (change == 3) {
-                change = change - nominal[1];
-                changes.add(nominal[1]);
-            }
-            if (change == 1) {
-                change = change - nominal[0];
-                changes.add(nominal[0]);
+        for (int coin : coins ) {
+            while(change >= coin) {
+                change = change - coin;
+                changes.add(coin);
             }
         }
         return changes;
